31ecf70a4f3e36e8ec41eeb5097f5ce5647c89df
du-nt <mahoagray@gmail.com>
2024-05-21 00:00:42 +0900

    test

--- vars/pipelineInclude.groovy
+++ vars/pipelineInclude.groovy
@@ -1,15 +1,12 @@
 def call(body) {
     pipeline {
-        stages {
-            stage("Prepare Build Environment") {
-                steps {
-                    script{
-                    withCredentials([file(credentialsId: "wif-config-file", variable: 'GC_KEY')]) {
-                        sh 'echo $GC_KEY'
-                        }
-                    }
-                }
-            }
+    agent { label 'linux' }
+    stages {
+      stage('Run') {
+        steps {
+          echo "Hello!"
         }
+      }
     }
+  }
 }
